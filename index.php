44e6af0fa59fb4ac5b441fb7ff1a8fae3efabab1
Ermian2 <rafael.home@live.com>
2017-09-19 20:07:47 -0300

    inserir e alterar

--- index.php
+++ index.php
@@ -17,12 +17,23 @@ require_once("config.php");
 //echo json_encode($search);
 
 //Carrega um usuário usando o login e a senha
+//$usuario = new Usuario();
+//$usuario->login("joao", "1234567890");
+//echo $usuario;
+/*
+//Criando um novo usuário
+$aluno = new Usuario("aluno", "@lun0");
+$aluno->insert();
+echo $aluno;
+*/
+
 $usuario = new Usuario();
-$usuario->login("joao", "1234567890");
+
+$usuario->loadbyId(8);
+
+$usuario->update("professor", "!@#$%^&*");
 
 echo $usuario;
-
-
 
 
 /*
